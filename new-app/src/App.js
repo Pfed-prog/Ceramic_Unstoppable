ad41b7cd822d543fc00d53fa8dba4c3743d666f1
Pavel Fedotov <66903336+Pfed-prog@users.noreply.github.com>
2021-12-09 12:34:35 +0300

    added ud

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,21 +1,64 @@
-import './App.css';
+import {useWeb3React} from '@web3-react/core'
+import {WalletConnectConnector} from '@web3-react/walletconnect-connector'
 import { useEffect, useState} from 'react';
 import CeramicClient from '@ceramicnetwork/http-client';
 import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver';
 import { ThreeIdConnect,  EthereumAuthProvider } from '@3id/connect'
 import { DID } from 'dids'
+
+import './App.css';
 import DataModels from './DataModels';
+import connectors from './connectors'
 
 const API_URL = 'https://ceramic-clay.3boxlabs.com';
 
 
 function App() {
-    
+    const {active, account, activate, deactivate} = useWeb3React()  
     const [ceramic, setCeramic] = useState();
     const [ethAddresses, setEthAddresses] = useState();
     const [ethereum, setEthereum] = useState();
     const [appStarted, setAppStarted] = useState(false);
   
+    function createConnectHandler(connectorId) {
+      return async () => {
+        try {
+          const connector = connectors[connectorId]
+  
+          if (
+            connector instanceof WalletConnectConnector &&
+            connector.walletConnectProvider?.wc?.uri
+          ) {
+            connector.walletConnectProvider = undefined
+          }
+  
+          await activate(connector)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }
+
+    function buttons() {
+      return (
+        <>
+          {Object.keys(connectors).map(v => (
+            <button key={v} onClick={createConnectHandler(v)}>
+            W Connect to {v}
+            </button>
+          ))}
+        </>
+      )
+    }
+
+/*     async function handleDisconnect() {
+      try {
+        deactivate()
+      } catch (error) {
+        console.error(error)
+      }
+    } */
+
     useEffect(() => {
       if(window.ethereum) {
         setEthereum(window.ethereum);
@@ -35,7 +78,7 @@ function App() {
       if(ethereum && ethAddresses) {
         (async () => {
           const newCeramic = new CeramicClient(API_URL);
-  
+
           const resolver = {
             ...ThreeIdResolver.getResolver(newCeramic),
           }
@@ -54,24 +97,28 @@ function App() {
       }
     }, [ethereum, ethAddresses]);
   
-    function getEthNeededPanel() {
-      return <div>
-       <h2> You need wallet 
-        <a href="https://metamask.io/" target="_blank" rel="noreferrer">
-          Try MetaMask
-        </a> </h2>
-      </div>;
-    }
+    function createConnectHandler(connectorId) {
+      return async () => {
+        try {
+          const connector = connectors[connectorId]
   
-    function getWaitingForEthPanel() {
-      return <div>
-        <h2>Waiting for Ethereum accounts...</h2>
-      </div>;
+          if (
+            connector instanceof WalletConnectConnector &&
+            connector.walletConnectProvider?.wc?.uri
+          ) {
+            connector.walletConnectProvider = undefined
+          }
+
+          await activate(connector)
+        } catch (error) {
+          console.error(error)
+        }
+      }
     }
-  
+
     function getWaitingForDIDPanel() {
       return <div>
-       <h2> Waiting for a decentralized ID...</h2>
+       <h2> Waiting for a decentralized ID :{ethAddresses}</h2>
       </div>
     }
   
@@ -83,19 +130,15 @@ function App() {
               </h1>
 
                 { 
-                  ethereum ? 
+                  active ? 
                   (
-                    ethAddresses ?  
-                    (
                       ceramic ?
-                      <button onClick={() => setAppStarted(true)}>Let&apos;s go !!</button> : 
+                      <button onClick={() => setAppStarted(true)}>Let&apos;s go !!</button> 
+                      : 
                       getWaitingForDIDPanel()
-                    ) 
-                    :
-                    getWaitingForEthPanel() 
                   )
                   :
-                  getEthNeededPanel()
+                  buttons()
                 }
               </div>
       );
