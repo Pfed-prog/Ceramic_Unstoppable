37de4681cad53fff5c68f3d7ac9f02bd5dec3dfd
pfed-prog <fedorovn19@gmail.com>
2022-08-08 21:17:16 +0200

    preparing for app revamp

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
-# Ceramic_Unstoppable with UD Login
-
-
 ![image](https://user-images.githubusercontent.com/66903336/146200677-e9ec8969-19cb-43cc-a2b0-5b6ccc7a3411.png)
-
 
 The app is available on https://ceramic-unstoppable.vercel.app/
 
 [YouTube Video](https://youtu.be/IVo6tN8BpOY)
 
+The write-up is also available on my blog: https://dspyt.com/simple-app-with-ceramic-data-model-and-unstoppable-domains/
+
 ## Overview of the App
 
-We implemented Unstoppable Domains Login Option.
-
-The application interacts with JS HTTP Client through a public Ceramic node. In addition we create a deterministic Ceramic Tile and resolve an unstoppable domain which we store in the decentralized profile of the user.
-
 Ceramic allows users to have complete ownership over their data by providing decentralized technologies for authentication and data storage.
+
+In this tutorial we set up an application to interact with JS HTTP Client through a public Ceramic node. In addition we create a deterministic Ceramic Tile and resolve an unstoppable domain which we store in the decentralized profile of the user.
+
+![](https://i.imgur.com/6zbdC3L.jpg?1)
+
+![](https://i.imgur.com/gcs0NJT.jpg?1)
+
+![](https://i.imgur.com/AcxIR8R.jpg?1)
+
+![](https://i.imgur.com/qyg4dep.jpg?1)
+
+## Requirements
+
+Node.js v14 and npm v6: https://developers.ceramic.network/build/javascript/http/
+
+## Libraries
+
+- @ceramicnetwork/stream-tile
+- @ceramicnetwork/3id-did-resolver
+- @3id/connect
+- dids
+- @unstoppabledomains/resolution
+- near-api-js
+
+## Additional Resources
+
+[Unstoppable Domains Docs](https://docs.unstoppabledomains.com/)
+
+[Ceramic Developers Quickstart](https://developers.ceramic.network/build/javascript/quick-start/)
+
+[Ceramic Tutorials](https://blog.ceramic.network/tag/tutorials/)
--- Unstoppable_Ceramic/src/App.js
+++ Unstoppable_Ceramic/src/App.js
@@ -1,166 +1,158 @@
-import {useWeb3React} from '@web3-react/core'
-import {WalletConnectConnector} from '@web3-react/walletconnect-connector'
-import { useEffect, useState} from 'react';
-import CeramicClient from '@ceramicnetwork/http-client';
-import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver';
-import { ThreeIdConnect,  EthereumAuthProvider } from '@3id/connect'
-import { DID } from 'dids'
+import { useWeb3React } from "@web3-react/core";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { useEffect, useState } from "react";
+import CeramicClient from "@ceramicnetwork/http-client";
+import ThreeIdResolver from "@ceramicnetwork/3id-did-resolver";
+import { ThreeIdConnect, EthereumAuthProvider } from "@3id/connect";
+import { DID } from "dids";
 
-import './css/App.css';
-import DataModels from './DataModels';
-import connectors from './connectors'
+import "./css/App.css";
+import DataModels from "./DataModels";
+import connectors from "./connectors";
 
-const API_URL = 'https://ceramic-clay.3boxlabs.com';
-
+const API_URL = "https://ceramic-clay.3boxlabs.com";
 
 function App() {
-    const {active, account, activate, deactivate} = useWeb3React()  
-    const [ceramic, setCeramic] = useState();
-    const [ethAddresses, setEthAddresses] = useState();
-    const [ethereum, setEthereum] = useState();
-    const [appStarted, setAppStarted] = useState(false);
-  
-    function createConnectHandler(connectorId) {
-      return async () => {
-        try {
-          const connector = connectors[connectorId]
-  
-          if (
-            connector instanceof WalletConnectConnector &&
-            connector.walletConnectProvider?.wc?.uri
-          ) {
-            connector.walletConnectProvider = undefined
-          }
-  
-          await activate(connector)
-        } catch (error) {
-          console.error(error)
+  const { active, account, activate, deactivate } = useWeb3React();
+  const [ceramic, setCeramic] = useState();
+  const [ethAddresses, setEthAddresses] = useState();
+  const [ethereum, setEthereum] = useState();
+  const [appStarted, setAppStarted] = useState(false);
+
+  function createConnectHandler(connectorId) {
+    return async () => {
+      try {
+        const connector = connectors[connectorId];
+
+        if (
+          connector instanceof WalletConnectConnector &&
+          connector.walletConnectProvider?.wc?.uri
+        ) {
+          connector.walletConnectProvider = undefined;
         }
+
+        await activate(connector);
+      } catch (error) {
+        console.error(error);
       }
-    }
+    };
+  }
 
-    function buttons() {
-      return (
-        <>
-          <button
-            onClick={createConnectHandler(Object.keys(connectors)[0])}>
-            <span>Login with Metamask</span>
-          </button>
-
-          <button
-            onClick={createConnectHandler(Object.keys(connectors)[1])}>
-            <span>Login with WalletConnect</span>
-          </button>
-
-          <button className="unst"
-            onClick={createConnectHandler(Object.keys(connectors)[2])}>
-            
-
-          </button>
-
-        </>
-      )
-    }
-
-    useEffect(() => {
-      if(active) {
-        setEthereum(window.ethereum);
-        (async() => {
-          try {
-            const addresses = await window.ethereum.request({ method: 'eth_requestAccounts'})
-            setEthAddresses(addresses);
-          }
-          catch(e) { 
-            console.log(e);
-          }
-        })();
-      }
-    }, [active]);
-  
-    useEffect(() => {
-      if(ethereum && ethAddresses) {
-        (async () => {
-          const newCeramic = new CeramicClient(API_URL);
-
-          const resolver = {
-            ...ThreeIdResolver.getResolver(newCeramic),
-          }
-          const did = new DID({ resolver })
-          newCeramic.did = did;
-          const threeIdConnect = new ThreeIdConnect()
-          const authProvider = new EthereumAuthProvider(ethereum, ethAddresses[0]);
-          await threeIdConnect.connect(authProvider)
-  
-          const provider = await threeIdConnect.getDidProvider();
-          newCeramic.did.setProvider(provider);
-          await newCeramic.did.authenticate();
-  
-          setCeramic(newCeramic);
-        })();
-      }
-    }, [ethereum, ethAddresses]);
-  
-    function createConnectHandler(connectorId) {
-      return async () => {
-        try {
-          const connector = connectors[connectorId]
-  
-          if (
-            connector instanceof WalletConnectConnector &&
-            connector.walletConnectProvider?.wc?.uri
-          ) {
-            connector.walletConnectProvider = undefined
-          }
-
-          await activate(connector)
-        } catch (error) {
-          console.error(error)
-        }
-      }
-    }
-
-    function getWaitingForDIDPanel() {
-      return <div>
-       <h2> Waiting for a decentralized ID :{account}</h2>
-      </div>
-    }
-  
-    function getLandingPage() {
-      return (
-        <div className="Init">
-              <h1>
-                Take Control of Your Data with T+D
-              </h1>
-
-                { 
-                  active ? 
-                  (
-                      ceramic ?
-                      <button onClick={() => setAppStarted(true)}>Let&apos;s go !!</button> 
-                      : 
-                      getWaitingForDIDPanel()
-                  )
-                  :
-                  buttons()
-                }
-              </div>
-      );
-    }
-
-    function getSkillsPage() {
-      return <DataModels ceramic={ceramic} setAppStarted={setAppStarted} />
-    }
-  
+  function buttons() {
     return (
-      <div>
-        {
-          (ceramic && appStarted) ?
-            getSkillsPage() :
-            getLandingPage()
-        }
-      </div>
-   
+      <>
+        <button onClick={createConnectHandler(Object.keys(connectors)[0])}>
+          <span>Login with Metamask</span>
+        </button>
+
+        <button onClick={createConnectHandler(Object.keys(connectors)[1])}>
+          <span>Login with WalletConnect</span>
+        </button>
+
+        <button
+          className="unst"
+          onClick={createConnectHandler(Object.keys(connectors)[2])}
+        ></button>
+      </>
     );
   }
+
+  useEffect(() => {
+    if (active) {
+      setEthereum(window.ethereum);
+      (async () => {
+        try {
+          const addresses = await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          setEthAddresses(addresses);
+        } catch (e) {
+          console.log(e);
+        }
+      })();
+    }
+  }, [active]);
+
+  useEffect(() => {
+    if (ethereum && ethAddresses) {
+      (async () => {
+        const newCeramic = new CeramicClient(API_URL);
+
+        const resolver = {
+          ...ThreeIdResolver.getResolver(newCeramic),
+        };
+        const did = new DID({ resolver });
+        newCeramic.did = did;
+        const threeIdConnect = new ThreeIdConnect();
+        const authProvider = new EthereumAuthProvider(
+          ethereum,
+          ethAddresses[0]
+        );
+        await threeIdConnect.connect(authProvider);
+
+        const provider = await threeIdConnect.getDidProvider();
+        newCeramic.did.setProvider(provider);
+        await newCeramic.did.authenticate();
+
+        setCeramic(newCeramic);
+      })();
+    }
+  }, [ethereum, ethAddresses]);
+
+  function createConnectHandler(connectorId) {
+    return async () => {
+      try {
+        const connector = connectors[connectorId];
+
+        if (
+          connector instanceof WalletConnectConnector &&
+          connector.walletConnectProvider?.wc?.uri
+        ) {
+          connector.walletConnectProvider = undefined;
+        }
+
+        await activate(connector);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }
+
+  function getWaitingForDIDPanel() {
+    return (
+      <div>
+        <h2> Waiting for a decentralized ID :{account}</h2>
+      </div>
+    );
+  }
+
+  function getLandingPage() {
+    return (
+      <div className="Init">
+        <h1>Take Control of Your Data with T+D</h1>
+
+        {active ? (
+          ceramic ? (
+            <button onClick={() => setAppStarted(true)}>
+              Let&apos;s go !!
+            </button>
+          ) : (
+            getWaitingForDIDPanel()
+          )
+        ) : (
+          buttons()
+        )}
+      </div>
+    );
+  }
+
+  function getSkillsPage() {
+    return <DataModels ceramic={ceramic} setAppStarted={setAppStarted} />;
+  }
+
+  return (
+    <div>{ceramic && appStarted ? getSkillsPage() : getLandingPage()}</div>
+  );
+}
 
 export default App;
